c9b4860db2eae1c85e97576e5de9aedac3b21882
Espen Skorstøl Gjernes <espens.gjernes@Espen-sin-MacBook-Pro.local>
2024-10-24 20:26:12 +0200

    Fixup

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export const App = () => {
       <p className="text-4xl text-center pt-4">Logo</p>
       <div className="flex justify-between items-center">
         <img className="h-40" src={leftFoot} />
-        <div className="pl-10">
-          <DownloadButton />
-        </div>
         <img className="h-48" src={rightFoot} />
+      </div>
+      <div className="flex justify-center items-center h-full">
+        <DownloadButton />
       </div>
       <div className="flex gap-4 h-full items-end p-4 justify-center">
         <Link className="text-[#424242]" to={`privacy`}>
