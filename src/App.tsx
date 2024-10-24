2e9768e7a84578355b5b6a6831245c9922f5b64a
Espen Skorstøl Gjernes <espens.gjernes@Espen-sin-MacBook-Pro.local>
2024-10-24 20:19:03 +0200

    Add some content and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import leftFoot from "./assets/leftFoot.svg";
+import rightFoot from "./assets/rightFoot.svg";
+import { DownloadButton } from "./components/DownloadButton";
+
 export const App = () => {
-  return <p>Konka</p>;
+  return (
+    <div className="flex flex-col h-screen">
+      <p className="text-4xl text-center pt-4">Logo</p>
+      <div className="flex justify-between items-center">
+        <img className="h-40" src={leftFoot} />
+        <div className="pl-10">
+          <DownloadButton />
+        </div>
+        <img className="h-48" src={rightFoot} />
+      </div>
+      <div className="flex gap-4 h-full items-end p-4 justify-center">
+        <Link className="text-[#424242]" to={`privacy`}>
+          Privacy
+        </Link>
+        <Link className="text-[#424242]" to={`contact`}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>Oops!</p>,
+  },
+  {
+    path: "contact",
+    element: <p>Contact</p>,
+  },
+  {
+    path: "privacy",
+    element: <p>Privacy</p>,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
