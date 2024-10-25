71c3f37bd796441790ec0f7e7ef2c8f50b1dfe1c
Espen Skorstøl Gjernes <espens.gjernes@Espen-sin-MacBook-Pro.local>
2024-10-25 12:19:42 +0200

    Add Apple App Banners

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { DownloadButton } from "./components/DownloadButton";
 
 export const App = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-full">
       <p className="text-4xl text-center pt-4">Logo</p>
       <div className="flex justify-between items-center">
         <img className="h-40" src={leftFoot} />
         <img className="h-48" src={rightFoot} />
       </div>
-      <div className="flex justify-center items-center h-full">
+      <div className="flex justify-center items-center">
         <DownloadButton />
       </div>
-      <div className="flex gap-4 h-full items-end p-4 justify-center">
+      <div className="flex gap-4 p-4 justify-center h-full items-end">
         <Link className="text-[#424242]" to={`privacy`}>
           Privacy
         </Link>
