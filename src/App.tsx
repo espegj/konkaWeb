c53f77e845d641a4aee30191af2dcea92756540a
Espen Skorstøl Gjernes <espens.gjernes@Espen-sin-MacBook-Pro.local>
2024-10-25 16:03:53 +0200

    New design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 import leftFoot from "./assets/leftFoot.svg";
 import rightFoot from "./assets/rightFoot.svg";
+import konkaLogo from "./assets/konka.svg";
+import topArm from "./assets/topArm.svg";
+import leftArm from "./assets/leftArm.svg";
 import { DownloadButton } from "./components/DownloadButton";
 
 export const App = () => {
   return (
     <div className="flex flex-col h-full">
-      <p className="text-4xl text-center pt-4">Logo</p>
-      <div className="flex justify-between items-center">
-        <img className="h-40" src={leftFoot} />
-        <img className="h-48" src={rightFoot} />
+      <div className="flex flex-col">
+        <img className="h-20 self-end" src={topArm} />
+        <img className="h-20 self-start" src={leftArm} />
+        <img className="h-48 self-end" src={rightFoot} />
+        <img className="h-40 self-start" src={leftFoot} />
       </div>
-      <div className="flex justify-center items-center">
+      <img
+        className="h-32 absolute top-28 mr-auto ml-auto text-center left-0 right-0"
+        src={konkaLogo}
+      />
+      <div className="flex flex-col gap-2 h-full items-center justify-end">
         <DownloadButton />
-      </div>
-      <div className="flex gap-4 p-4 justify-center h-full items-end">
-        <Link className="text-[#424242]" to={`privacy`}>
-          Privacy
-        </Link>
-        <Link className="text-[#424242]" to={`contact`}>
-          Contact
-        </Link>
+        <div className="flex gap-4 p-4 justify-center text-[#6498ED]">
+          <Link to={`privacy`}>Privacy</Link>
+          <Link to={`contact`}>Contact</Link>
+        </div>
       </div>
     </div>
   );
